6209f2ecd7d4cd805c0954459fca922adf85262e
Torben Haack <tohaack@pm.me>
2024-06-09 18:51:00 +0200

    💡 commented out firefox profile - not yet supported

--- home/firefox.nix
+++ home/firefox.nix
@@ -4,7 +4,7 @@
   programs.firefox = {
     enable = true;
     package = pkgs.librewolf;
-    profiles.torben = {
+    /* profiles.torben = {
       bookmarks = [
         {
           name = "wikipedia";
@@ -12,7 +12,7 @@
           keyword = "wiki";
           url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
         }
-      ]
-    };
+      ];
+    }; */
   };
 }
