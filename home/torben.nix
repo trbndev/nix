e525ef7ae46cc96630c62372adf252ad146198e6
Torben Haack <tohaack@pm.me>
2024-06-09 15:15:17 +0200

    ➕ Add chromium extensions

--- home/torben.nix
+++ home/torben.nix
@@ -27,6 +27,14 @@
 
   programs.chromium = {
     enable = true;
+    extensions = [
+      { id = "epcnnfbjfcgphgdmggkamkmgojdagdnn"; } # uBlock Origin
+      { id = "edibdbjcniadpccecjdfdjjppcpchdlm"; } # I still don't care about cookies
+      { id = "pkehgijcmpdhfbdbbnkijodmdjhbjlgp"; } # Privacy Badger
+      { id = "ghmbeldphafepmbegfdlkpapadhbakde"; } # Proton Pass
+      { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # Sponsor Block
+      { id = "ifnkdbpmgkdbfklnbfidaackdenlmhgh"; } # PopUpOFF
+    ];
   };
 
   programs.git = {
