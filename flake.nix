cfec93ece3bb6310edd757a7529bc944e5ef96bf
Torben Haack <tohaack@pm.me>
2024-06-09 14:45:43 +0200

    ✨ init commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "@trbndev/nix - Nix configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ { self, nixpkgs, home-manager, ... }: 
+  let 
+    inherit (self) outputs;
+  in {
+    nixosConfigurations = {
+      ideapad = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs outputs; };
+        modules = [
+          ./hosts/ideapad/configuration.nix
+        ];
+      };
+    };
+
+    homeConfigurations = {
+      "torben@ideapad" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64_linux;
+        extraSpecialArgs = { inherit inputs outputs; };
+        modules = [
+          ./hosts/ideapad/home.nix
+        ];
+      };
+    };
+  };
+}
