326d4eeae14a7ef7a61152059e85bcf357976775
Torben Haack <tohaack@pm.me>
2024-06-09 15:35:58 +0200

    🔧 update vscode extensions

--- home/torben.nix
+++ home/torben.nix
@@ -53,9 +53,8 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      vscodevim.vim
-      yzhang.markdown-all-in-one
+      jnoortheen.nix-ide
+      seatonjiang.gitmoji-vscode
     ];
   };
 
