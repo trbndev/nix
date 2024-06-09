37383a06aaa3a02cd342327ed6b598761abd2614
Torben Haack <tohaack@pm.me>
2024-06-09 18:45:52 +0200

    ✨ add bookmarks to firefox settings

--- home/firefox.nix
+++ home/firefox.nix
@@ -4,6 +4,15 @@
   programs.firefox = {
     enable = true;
     package = pkgs.librewolf;
-    # profiles.torben = {};
+    profiles.torben = {
+      bookmarks = [
+        {
+          name = "wikipedia";
+          tags = [ "wiki" ];
+          keyword = "wiki";
+          url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+        }
+      ]
+    };
   };
 }
