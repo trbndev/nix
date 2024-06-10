36fa9d959ee2807e2966a79e42cf75ea49b5f8be
Torben Haack <tohaack@pm.me>
2024-06-10 11:52:28 +0200

    ⬆️ upgrade home-manager to unstable

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
