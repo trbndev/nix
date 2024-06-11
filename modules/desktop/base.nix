6ae560154d25fd543eba423c9cd77c90e89eb0e4
Torben Haack <tohaack@pm.me>
2024-06-11 19:51:58 +0200

    🩹 remove extra tab

--- modules/desktop/base.nix
+++ modules/desktop/base.nix
@@ -1,7 +1,7 @@
 { ... }:
 
 {
-    # Enable the X11 windowing system.
+  # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
