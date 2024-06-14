328e4e88838f413448b8333e3016c9d1571d0b66
Torben Haack <tohaack@pm.me>
2024-06-14 12:35:28 +0200

    ✨ add oh-my-posh template

--- home/config/shell.nix
+++ home/config/shell.nix
@@ -6,5 +6,10 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    # initExtra = "eval \"$(oh-my-posh init zsh)\"";
   };
+  # programs.oh-my-posh = {
+  #  enable = true;
+  #  enableZshIntegration = true;
+  # };
 }
