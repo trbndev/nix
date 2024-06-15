a9585c04c4477d23a4ad345c0dc20badfa7a83e5
Torben Haack <tohaack@pm.me>
2024-06-15 17:55:09 +0200

    🐛 actually install gnome extensions

--- modules/system/desktop/gnome.nix
+++ modules/system/desktop/gnome.nix
@@ -4,4 +4,8 @@
   # Enable the Gnome Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs.gnomeExtensions; [
+    blur-my-shell
+  ];
 }
