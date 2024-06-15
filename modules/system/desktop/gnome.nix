d297003e9b0271fb794329c58c3b2f8fe92fb12a
Torben Haack <tohaack@pm.me>
2024-06-15 18:21:47 +0200

    ✨ customize gnome

--- modules/system/desktop/gnome.nix
+++ modules/system/desktop/gnome.nix
@@ -5,6 +5,40 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  # Choose which default packages to remove
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    baobab      # disk usage analyzer
+    cheese      # photo booth
+    # eog         # image viewer
+    epiphany    # web browser
+    # gedit       # text editor
+    # simple-scan # document scanner
+    # totem       # video player
+    # yelp        # help viewer
+    # evince      # document viewer
+    # file-roller # archive manager
+    geary       # email client
+    seahorse    # password manager
+
+    # these should be self explanatory
+    # gnome-calculator 
+    # gnome-calendar 
+    # gnome-characters 
+    gnome-clocks
+    gnome-contacts
+    # gnome-font-viewer 
+    # gnome-logs
+    # gnome-maps 
+    # gnome-music 
+    # gnome-photos 
+    # gnome-screenshot
+    # gnome-system-monitor 
+    # gnome-weather 
+    # gnome-disk-utility 
+    # pkgs.gnome-connections
+  ];
+
+  # Install Gnome Extensions
   environment.systemPackages = with pkgs.gnomeExtensions; [
     blur-my-shell
     dash-to-dock
