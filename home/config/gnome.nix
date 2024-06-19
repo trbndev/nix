770bd71146df8860f4ca36ac496717476e62b0ac
Torben Haack <tohaack@pm.me>
2024-06-19 20:11:25 +0200

    ✨ gtk theming

--- home/config/gnome.nix
+++ home/config/gnome.nix
@@ -1,12 +1,23 @@
 { pkgs, ... }:
 
 {
+  gtk.enable = true;
+  
+  gtk.cursorTheme.package = pkgs.bibata-cursors;
+  gtk.cursorTheme.name = "Bibata-Modern-Ice";
+  
+  gtk.theme.package = pkgs.adw-gtk3;
+  gtk.theme.name = "adw-gtk3";
+
+  gtk.iconTheme.package = pkgs.gruvboxPlus;
+  gtk.iconTheme.name = "GruvboxPlus";
+
   dconf = {
     enable = true;
   
     settings = {
       "org/gnome/shell" = {
-        disable-user-extensions = false; # enables user extensions
+        disable-user-extensions = false;
         enabled-extensions = [
           # Put UUIDs of extensions that you want to enable here.
           # If the extension you want to enable is packaged in nixpkgs,
