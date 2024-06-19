3c4098d36390a08960780d6d085097d117b008c1
Torben Haack <tohaack@pm.me>
2024-06-19 20:29:38 +0200

    ✨ configure gnome

--- home/config/gnome.nix
+++ home/config/gnome.nix
@@ -16,40 +16,30 @@
     enable = true;
   
     settings = {
+      # Gnome Core Settings
       "org/gnome/shell" = {
         disable-user-extensions = false;
         enabled-extensions = [
-          # Put UUIDs of extensions that you want to enable here.
-          # If the extension you want to enable is packaged in nixpkgs,
-          # you can easily get its UUID by accessing its extensionUuid
-          # field (look at the following example).
           pkgs.gnomeExtensions.gsconnect.extensionUuid
           pkgs.gnomeExtensions.blur-my-shell.extensionUuid
           pkgs.gnomeExtensions.dash-to-dock.extensionUuid
         ];
-      };
 
-      # Configure individual extensions
-      "org/gnome/shell/extensions/blur-my-shell" = {
-        brightness = 0.75;
-        noise-amount = 0;
-      };
-
-
-      "org/gnome/shell/extensions/dash-to-dock" = {
-        dash-max-icon-size = 32;
+        favorite-apps = [
+          "org.gnome.Nautilus.desktop"
+          "firefox.desktop"
+        ];
       };
 
       "org/gnome/desktop/wm/preferences" = {
         button-layout = "appmenu:minimize,maximize,close";
       };
 
-      "org/gnome/shell" = {
-        favorite-apps = [
-          "org.gnome.Nautilus.desktop"
-          "firefox.desktop"
-        ];
+      # Extension Settings
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        dash-max-icon-size = 32;
       };
+
     };
   };
 }
